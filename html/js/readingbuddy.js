354dec324fc31fdfa9cf995cd9d6aa3058d1d42c
Jesper Pedersen <jesper@yepz.dk>
2019-04-26 14:10:50 +0200

    Spilt js files

--- html/js/readingbuddy.js
+++ html/js/readingbuddy.js
@@ -81,7 +81,7 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 1);
+/******/ 	return __webpack_require__(__webpack_require__.s = 0);
 /******/ })
 /************************************************************************/
 /******/ ([
@@ -134,100 +134,6 @@ function raiseMemoryEventTest() {
   }, function (error) {
     console.log(error);
   })
-}
-
-/***/ }),
-/* 1 */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _js_readingbuddy_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(0);
-/* harmony import */ var _js_readingbuddy_js__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_js_readingbuddy_js__WEBPACK_IMPORTED_MODULE_0__);
-
-
-// Create readtime btns
-
-var readTime = [1,20,30];
-
-for (let i = 0; i < readTime.length; i++) {
-    const time = readTime[i];
-    document.getElementById('readTime' + time).addEventListener('click', function(){
-        pageChoice(time/5); // Divide by 5 to speed up test
-    });
-}
-
-// Choices section
-
-function pageChoice(time) {
-    hideShow('pageChoice', 'pageTimer');
-    pageTimer(time);
-};
-
-
-// Countdown section
-
-function pageTimer(time) {
-    countdown( 'ten-countdown', time, 0 );
-};
-
-// Timer functions
-
-function countdown( elementName, minutes, seconds )
-{
-    var element, endTime, hours, mins, msLeft, time;
-
-    function twoDigits( n )
-    {
-        return (n <= 9 ? '0' + n : n);
-    }
-
-    function updateTimer()
-    {
-        msLeft = endTime - (+new Date);
-        if ( msLeft < 1000 ) {
-            hideShow('pageTimer', 'pageQuestion');
-        } else {
-            time = new Date( msLeft );
-            hours = time.getUTCHours();
-            mins = time.getUTCMinutes();
-            element.innerHTML = (hours ? hours + ':' + twoDigits( mins ) : mins) + ':' + twoDigits( time.getUTCSeconds() );
-            setTimeout( updateTimer, time.getUTCMilliseconds() + 500 );
-        }
-    }
-
-    element = document.getElementById( elementName );
-    endTime = (+new Date) + 1000 * (60*minutes + seconds) + 500;
-    updateTimer();
-}
-
-// Create question btns
-
-var questions = ['Hest','Sko','Trappe'];
-
-for (let i = 0; i < questions.length; i++) {
-    const question = questions[i];
-    document.getElementById('question' + question).addEventListener('click', function(){
-        pageQuestion();
-    });
-}
-
-// Question section
-
-function pageQuestion() {
-    hideShow('pageQuestion', 'pageChoice');
-}
-
-// Hide/show sections
-
-function hideShow (hide, show) {
-    // hide
-    const h = document.getElementById(hide);
-    h.classList.toggle('d-none');
-
-    // Show
-    const s = document.getElementById(show);
-    s.classList.toggle('d-none');
 }
 
 /***/ })
--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,88 @@
+// const path = require('path');
+// const webpack = require('webpack');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   plugins: [
+//     new webpack.ProvidePlugin({
+//       $: "jquery",
+//       jQuery: "jquery"
+//     })
+//   ],
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'html/js/')
+//   }
+// };
+
 const path = require('path');
-const webpack = require('webpack');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
-  plugins: [
-    new webpack.ProvidePlugin({
-      $: "jquery",
-      jQuery: "jquery"
-    })
-  ],
+  entry: {
+    'main': './src/index.js',
+    'readingbuddy': './src/js/readingbuddy.js',
+  },
   output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'html/js/')
-  }
+    path: path.resolve(__dirname, 'html/js/'),
+    filename: "[name].js",
+  },
+  module: {
+    rules: [
+    //   {
+    //     test: /\.scss$/,
+    //     use: [
+    //       {
+    //         loader: MiniCssExtractPlugin.loader
+    //       },
+    //       "css-loader",
+    //       'sass-loader'
+    //     ]
+    //   },
+    //   {
+    //     test: /\.m?js$/,
+    //     exclude: /(node_modules|bower_components)/,
+    //     use: {
+    //       loader: 'babel-loader',
+    //       options: {
+    //         presets: ['@babel/preset-env']
+    //       }
+    //     }
+    //   },
+      {
+        test: require.resolve('jquery'),
+        use: [{
+          loader: 'expose-loader',
+          options: 'jQuery'
+        },{
+          loader: 'expose-loader',
+          options: '$'
+        }]
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        cssProcessorPluginOptions: {
+          preset: ['default', { discardComments: { removeAll: true } }],
+        },
+      }),
+    //   new UglifyJsPlugin({
+    //     uglifyOptions: {
+    //       output: {
+    //         comments: false,
+    //       },
+    //     },
+    //   })
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
 };
-
